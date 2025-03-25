71ea1e99873c6bb5fc0dca3a7a810712d6f4268e
sandarva3 <sandarvapoudel182@gmail.com>
2025-03-25 22:10:50 +0545

    postgresql integration for spatial indexing

--- core/settings.py
+++ core/settings.py
@@ -33,7 +33,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'travel',
     'rest_framework',
-    
+    'django.contrib.gis',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -78,8 +79,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'travel',
+        'USER': 'sandarva',
+        'PASSWORD': 'sandarva3',
+        'HOST': 'localhost',  # '127.0.0.1'
+        'PORT': '5432',
     }
 }
 
